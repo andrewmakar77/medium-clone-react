b4d30376521a256ef098fd6a186f151433a653e2
Andrew Makar <andrew.makar77@gmail.com>
2020-04-27 18:44:20 +0300

    Add basic routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PageLayout } from 'components';
+
+const App = () => {
+  return (
+    <Router>
+      <PageLayout/>
+    </Router>
+  );
+}
+
+export default App;
+
 
 ReactDOM.render(
   <React.StrictMode>
