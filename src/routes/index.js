d9420194059e408d2e451d90f3cd684b0e8726cd
Andrew Makar <andrew.makar77@gmail.com>
2020-04-27 21:53:42 +0300

    Add topbar, change favicon, remove unnecessary files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { FEED, ARTICLES } from 'constants/routes';
+import { HOME, ARTICLES } from 'constants/routes';
 import { GlobalFeeds, Articles } from 'pages';
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route path={FEED} component={GlobalFeeds} exact />
+      <Route path={HOME} component={GlobalFeeds} exact />
       <Route path={ARTICLES} component={Articles} />
     </Switch>
   )
